14c96dc3f1c762bb7819d7d92b1aeb6d806967ab
Tak-John Cheung <tak_john@hotmail.com>
2020-09-23 13:41:14 +0100

    added callout

--- scene/src/frisbee.ts
+++ scene/src/frisbee.ts
@@ -1,8 +1,12 @@
-import { Sound } from './sounds'
-import * as ui from '../node_modules/@dcl/ui-utils/index'
-import { createFloatingText } from './floatingText'
-import { alteredUserName, dataType } from './wsConnection'
-import { catchHint, streakCounter } from './game'
+import { Sound } from "./sounds"
+import * as ui from "../node_modules/@dcl/ui-utils/index"
+import { createFloatingText } from "./floatingText"
+import { alteredUserName, dataType } from "./wsConnection"
+import { catchHint, streakCounter } from "./game"
+import { Callout, calloutGroup } from "./callout"
+
+// Create callout
+const callout = new Callout(new GLTFShape("models/callout.glb"), new Transform({ position: new Vector3(24, 1.5, 24) }))
 
 const X_OFFSET = 0
 const Y_OFFSET = 0.5
@@ -12,8 +16,8 @@ const FIXED_TIME_STEPS = 1.0 / 60.0 // seconds
 const MAX_TIME_STEPS = 3
 const SHOOT_VELOCITY = 50
 
-const shootSound = new Sound(new AudioClip('sounds/shoot.mp3'))
-const recallSound = new Sound(new AudioClip('sounds/recall.mp3'))
+const shootSound = new Sound(new AudioClip("sounds/shoot.mp3"))
+const recallSound = new Sound(new AudioClip("sounds/recall.mp3"))
 
 export class Frisbee extends Entity {
   isFired: boolean = false
@@ -27,15 +31,10 @@ export class Frisbee extends Entity {
   world: CANNON.World
   socket: WebSocket
   ground: CANNON.Body
-  constructor(
-    transform: Transform,
-    world: CANNON.World,
-    socket: WebSocket,
-    ground: CANNON.Body
-  ) {
+  constructor(transform: Transform, world: CANNON.World, socket: WebSocket, ground: CANNON.Body) {
     super()
     engine.addEntity(this)
-    this.addComponent(new GLTFShape('models/disc.glb'))
+    this.addComponent(new GLTFShape("models/disc.glb"))
     this.addComponent(transform)
 
     this.world = world
@@ -44,11 +43,11 @@ export class Frisbee extends Entity {
 
     // Glow setup
     this.blueGlow.addComponent(new Transform())
-    this.blueGlow.addComponent(new GLTFShape('models/blueGlow.glb'))
+    this.blueGlow.addComponent(new GLTFShape("models/blueGlow.glb"))
     this.blueGlow.setParent(this)
 
     this.orangeGlow.addComponent(new Transform())
-    this.orangeGlow.addComponent(new GLTFShape('models/orangeGlow.glb'))
+    this.orangeGlow.addComponent(new GLTFShape("models/orangeGlow.glb"))
     this.orangeGlow.setParent(this)
 
     this.setGlow(false)
@@ -60,31 +59,25 @@ export class Frisbee extends Entity {
 
           this.playerPickUp(this.getComponent(Transform).position.clone())
         },
-        { hoverText: 'Pick up', distance: 6, button: ActionButton.PRIMARY }
+        { hoverText: "Pick up", distance: 6, button: ActionButton.PRIMARY }
       )
     )
 
     this.body = new CANNON.Body({
       mass: 1.25, // kg
-      position: new CANNON.Vec3(
-        transform.position.x,
-        transform.position.y,
-        transform.position.z
-      ), // m
+      position: new CANNON.Vec3(transform.position.x, transform.position.y, transform.position.z), // m
       shape: new CANNON.Sphere(0.2), // m (Create sphere shaped body with a radius of 0.2)
     })
 
-    const translocatorPhysicsMaterial: CANNON.Material = new CANNON.Material(
-      'translocatorMaterial'
-    )
+    const translocatorPhysicsMaterial: CANNON.Material = new CANNON.Material("translocatorMaterial")
 
     this.body.material = translocatorPhysicsMaterial // Add bouncy material to translocator body
     this.body.linearDamping = 0.4 // Round bodies will keep translating even with friction so you need linearDamping
     this.body.angularDamping = 0.4 // Round bodies will keep rotating even with friction so you need angularDamping
 
-    this.body.addEventListener('collide', (e) => {
+    this.body.addEventListener("collide", (e) => {
       if (e.body == this.ground) {
-        log('Frisbee Collided w ground')
+        log("Frisbee Collided w ground")
         this.hitGround = true
         catchHint.uiText.visible = false
       }
@@ -93,16 +86,11 @@ export class Frisbee extends Entity {
     world.addBody(this.body) // Add body to the world
   }
 
-  // Switches between the glows
   setGlow(isFired: boolean): void {
     if (isFired) {
       this.isFired = isFired
-      this.blueGlow.getComponent(Transform).scale.setAll(0)
-      this.orangeGlow.getComponent(Transform).scale.setAll(1)
     } else {
       this.isFired = isFired
-      this.blueGlow.getComponent(Transform).scale.setAll(1)
-      this.orangeGlow.getComponent(Transform).scale.setAll(0)
     }
   }
   setPos(pos: Vector3, rot: Quaternion, holding?: boolean) {
@@ -128,6 +116,7 @@ export class Frisbee extends Entity {
     this.isFired = false
     recallSound.getComponent(AudioSource).playOnce()
     catchHint.uiText.visible = false
+    if (calloutGroup.entities.length > 0) callout.removeCallout()
 
     this.body.velocity.setZero()
     this.body.angularVelocity.setZero()
@@ -135,23 +124,12 @@ export class Frisbee extends Entity {
     this.getComponent(Transform).position.set(X_OFFSET, Y_OFFSET, Z_OFFSET)
     this.getComponent(Transform).rotation = Quaternion.Zero()
     this.body.position = new CANNON.Vec3(X_OFFSET, Y_OFFSET, Z_OFFSET)
-    //this.body.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 0, 0), 0)
 
     if (pos.y > 1.5) {
-      ui.displayAnnouncement(
-        'Wow!',
-        5,
-        false,
-        Color4.FromHexString('#f2ff3bff')
-      )
+      ui.displayAnnouncement("Wow!", 5, false, Color4.FromHexString("#f2ff3bff"))
       streakCounter.increase()
     } else if (!this.hitGround) {
-      ui.displayAnnouncement(
-        'Good catch!',
-        5,
-        false,
-        Color4.FromHexString('#f2ff3bff')
-      )
+      ui.displayAnnouncement("Good catch!", 5, false, Color4.FromHexString("#f2ff3bff"))
       streakCounter.increase()
     } else {
       streakCounter.set(0)
@@ -183,17 +161,11 @@ export class Frisbee extends Entity {
     this.getComponent(GLTFShape).visible = false
 
     if (pos.y > 1.5) {
-      createFloatingText('Wow!', pos, 0.5, 2, Color3.FromHexString('#f2ff3bff'))
+      createFloatingText("Wow!", pos, 0.5, 2, Color3.FromHexString("#f2ff3bff"))
     } else if (!this.hitGround) {
-      createFloatingText(
-        'Good Catch!',
-        pos,
-        0.5,
-        2,
-        Color3.FromHexString('#f2ff3bff')
-      )
+      createFloatingText("Good Catch!", pos, 0.5, 2, Color3.FromHexString("#f2ff3bff"))
     } else {
-      createFloatingText('Picked frisbee up', pos, 0.5, 2)
+      createFloatingText("Picked frisbee up", pos, 0.5, 2)
     }
     streakCounter.set(streak)
   }
@@ -220,16 +192,8 @@ export class Frisbee extends Entity {
 
     // Shoot
     this.body.applyImpulse(
-      new CANNON.Vec3(
-        shootDirection.x * SHOOT_VELOCITY,
-        shootDirection.y * SHOOT_VELOCITY,
-        shootDirection.z * SHOOT_VELOCITY
-      ),
-      new CANNON.Vec3(
-        this.body.position.x,
-        this.body.position.y,
-        this.body.position.z
-      )
+      new CANNON.Vec3(shootDirection.x * SHOOT_VELOCITY, shootDirection.y * SHOOT_VELOCITY, shootDirection.z * SHOOT_VELOCITY),
+      new CANNON.Vec3(this.body.position.x, this.body.position.y, this.body.position.z)
     )
   }
   otherThrow(pos: Vector3, rot: Quaternion, shootDirection: Vector3) {
@@ -239,25 +203,16 @@ export class Frisbee extends Entity {
     this.isFired = true
     this.hitGround = false
     catchHint.uiText.visible = true
-    //shootSound.getComponent(AudioSource).playOnce()
 
     this.getComponent(GLTFShape).visible = true
     this.setParent(null)
-
     engine.addSystem(new shootDiscSystem(this))
 
     this.getComponent(Transform).position.copyFrom(pos)
-    //this.getComponent(Transform).rotation.copyFrom(rot)
-
     this.body.position = new CANNON.Vec3(pos.x, pos.y, pos.z)
-    //this.body.quaternion = new CANNON.Quaternion(rot.x, rot.y, rot.z, rot.w)
 
     this.body.applyImpulse(
-      new CANNON.Vec3(
-        shootDirection.x * SHOOT_VELOCITY,
-        shootDirection.y * SHOOT_VELOCITY,
-        shootDirection.z * SHOOT_VELOCITY
-      ),
+      new CANNON.Vec3(shootDirection.x * SHOOT_VELOCITY, shootDirection.y * SHOOT_VELOCITY, shootDirection.z * SHOOT_VELOCITY),
       new CANNON.Vec3(pos.x, pos.y, pos.z)
     )
   }
@@ -271,9 +226,7 @@ class shootDiscSystem implements ISystem {
   update(dt: number): void {
     if (this.frisbee.isFired) {
       this.frisbee.world.step(FIXED_TIME_STEPS, dt, MAX_TIME_STEPS)
-      this.frisbee
-        .getComponent(Transform)
-        .position.copyFrom(this.frisbee.body.position)
+      this.frisbee.getComponent(Transform).position.copyFrom(this.frisbee.body.position)
     } else {
       engine.removeSystem(this)
     }
